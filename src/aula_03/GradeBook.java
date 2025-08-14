416392797de2a6a67de29b5e30b8c5ee09d02083
Lucas Oliverio Dos Santos <lucas.45324@alunosatc.edu.br>
2025-08-14 19:37:46 -0300

    teste

---
+++ src/aula_03/GradeBook.java
@@ -0,0 +1,7 @@
+package aula_03;
+
+public class GradeBook {
+    public void showMessage() {
+        System.out.println("hello");
+    }
+}
---
+++ src/aula_03/GradeBookTest.java
@@ -0,0 +1,8 @@
+package aula_03;
+
+public class GradeBookTest {
+    public static void main(String[] args) {
+        GradeBook gradeBook = new GradeBook();
+        gradeBook.showMessage();
+    }
+}
